400eba1df7d7844a836f9b3b8e291807751e0fba
guyuezhai <guyue_fei@163.com>
2020-10-13 23:46:56 +0800

    :ambulance: 增加jsonp实现

---
+++ blog/经典手写面试题/apply.js
@@ -0,0 +1,26 @@
+// apply 的实现
+
+// apply 的实现与call 的实现有许多相似之处，传参略有不同，call 可以有2个以上参数，apply最多有两个参数，且第二个参数只能是数组或类数组；
+// fun.call(context,arg1,arg2,arg3,...) : 第一个参数是 this 指向的对象，其他参数依次传入
+// fun.apply(context,[args]) : 第一个参数是 this 指向的对象，第二个参数是数组或者类数组
+
+Function.prototype.myApply = function (context,args){
+
+    // 判断传入的第一个参数是否存在，如不存在，在非严格模式下根据运行环境 取window 或者 global
+    if(!context){
+        context = typeof window !== 'undefined' ? window : global;
+    }
+    // this的指向是当前函数fun
+    context.fun = this;
+
+    let result
+    //根据第二个参数返回不同的执行结果
+    if(!args){
+        result = context.fun()
+    }else{
+        result = context.fun(...args)
+    }
+    //原本context并不存在fun属性，函数执行结束后删除fun
+    delete context.fun
+    return result
+}
---
+++ blog/经典手写面试题/bind.js
@@ -0,0 +1,13 @@
+// bind 实现
+// bind 返回的是一个函数
+Function.prototype.myBind = function(){
+    let [context,args] = [...arguments]
+    let _this = this;
+    return function Fun(){
+        //考虑使用new的情况
+        if(this instanceof Fun){
+            return new _this(...args,...arguments)
+        }
+        return _this.apply(context,args.concat(arguments))
+    }
+}
