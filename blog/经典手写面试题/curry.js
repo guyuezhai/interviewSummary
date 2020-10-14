23ba9177d70bb007b51172ade24d8d44ae23c866
guyuezhai <guyue_fei@163.com>
2020-10-14 22:36:43 +0800

    :sparkles: 增加柯里化 深浅拷贝

---
+++ blog/经典手写面试题/curry.js
@@ -0,0 +1,27 @@
+/** 
+ * 柯里化
+ * 柯里化（Currying）是把接受多个参数的函数变换成接受一个单一参数(最初函数的第一个参数)的函数，并且返回接受余下的参数且返回结果的新函数的技术。
+*/
+
+function curry(fn,...args) {
+    //如果传入参数长度小于函数总参数长度时返回一个函数继续递归柯里化，否则执行函数
+    return fn.length>args.length?(...params)=>curry(fn,...args,...params):fn(...args)
+}
+
+//与上面方法同理
+function addcurry(x) {
+    return function (y) {
+        return function (z) {
+            return x+y+z
+        }
+    }
+}
+
+function add(x,y,z) {
+    return x+y+z
+}
+
+let result = curry(add,3)
+let result2 = addcurry(3)
+console.log('result',result(1)(2),result(4)(5)) // 6 12
+console.log('result2',result2(1)(2),result2(4)(5)) // 6 12
